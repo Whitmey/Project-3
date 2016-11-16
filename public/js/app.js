f7720ebaa995dd0bcb2b30d98b96774554f2a551
Aaron <apurjh@gmail.com>
2016-11-16 11:18:49 +0000

    food model in src

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
-"use strict";console.log("JS loaded");
+"use strict";function Food(o){return new o("/foods/:id",{id:"@_id"},{update:{method:"PUT"}})}console.log("JS loaded"),angular.module("foodApp").factory("Food",Food),Food.$inject=["$resource"];
 //# sourceMappingURL=app.js.map
