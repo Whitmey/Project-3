9a3b431a627c944544c7c682021d78b2801580a0
Elliot Brock <ell_304@msn.com>
2016-11-16 17:23:11 +0000

    facebook auth working

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth');
 const secureRoute = require('../lib/secureRoute');
-// const oauthController = require('../controllers/oauth');
+const oauthController = require('../controllers/oauth');
 const foodsController = require('../controllers/foods');
 
 
 router
   .post('/login', authController.login)
-  .post('/register', authController.register);
-  // .post('/auth/facebook', oauthController.facebook);
+  .post('/register', authController.register)
+  .post('/auth/facebook', oauthController.facebook);
 
 
 router.route('/foods')
