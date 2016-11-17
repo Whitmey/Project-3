ef15d62c47aed72964a86227ec72d4835d701cdb
Curtis Osano <osanocurt@hotmail.co.uk>
2016-11-17 14:19:38 +0000

    chart and preogress bar added

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -30,4 +30,47 @@ function MainController($auth, $state, $rootScope) {
   $rootScope.$on('stateChangeStart', secureState);
 
   main.logout = logout;
+
+  function createChart() {
+
+    const ctx = document.getElementById('myChart');
+    const myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: ['Fibre', 'Protein', 'Carbohydrate', 'Fats', 'Water'],
+        datasets: [{
+          label: 'Foods',
+          data: [12, 30, 200, 20, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+  main.createChart= createChart;
+  main.createChart();
 }
