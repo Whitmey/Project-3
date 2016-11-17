d2d74df214cb5d1f14d852ab4313772eac4ef85d
Will Whitmey <w.whitmey@outlook.com>
2016-11-17 18:41:29 +0000

    added countdown

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,5 +1,6 @@
 angular.module('foodApp')
-  .controller('MainController', MainController);
+  .controller('MainController', MainController)
+  .controller('CountdownController', CountdownController);
 
 
 MainController.$inject = ['$auth', '$state', '$rootScope'];
@@ -72,3 +73,49 @@ function MainController($auth, $state, $rootScope) {
   main.createChart= createChart;
   main.createChart();
 }
+
+CountdownController.$inject = [];
+ function CountdownController() {
+   function getTimeRemaining(endtime) {
+     var t = Date.parse(endtime) - Date.parse(new Date());
+     var seconds = Math.floor((t / 1000) % 60);
+     var minutes = Math.floor((t / 1000 / 60) % 60);
+     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+     var days = Math.floor(t / (1000 * 60 * 60 * 24));
+     return {
+       'total': t,
+       'days': days,
+       'hours': hours,
+       'minutes': minutes,
+       'seconds': seconds
+     };
+   }
+
+   function initializeClock(id, endtime) {
+     var clock = document.getElementById(id);
+     var daysSpan = clock.querySelector('.days');
+     var hoursSpan = clock.querySelector('.hours');
+     var minutesSpan = clock.querySelector('.minutes');
+     var secondsSpan = clock.querySelector('.seconds');
+
+     function updateClock() {
+       var t = getTimeRemaining(endtime);
+
+       daysSpan.innerHTML = t.days;
+       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+       if (t.total <= 0) {
+         clearInterval(timeinterval);
+       }
+     }
+
+     updateClock();
+     var timeinterval = setInterval(updateClock, 1000);
+   }
+
+   var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+   initializeClock('clockdiv', deadline);
+
+ }
