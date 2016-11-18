84e204cfe8d25720c1a394e7f2f8f23fb68ad714
Aaron <apurjh@gmail.com>
2016-11-18 16:49:22 +0000

    removed to duplicate

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -31766,6 +31766,7 @@ $provide.value("$locale", {
 })(window);
 
 !window.angular.$$csp().noInlineStyle && window.angular.element(document.head).prepend('<style type="text/css">@charset "UTF-8";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}</style>');
+
 /**
  * State-based routing for AngularJS
  * @version v0.3.2
