6d42e8dd59aaf02937dad6b4d78a02f1e944ec88
Elliot Brock <ell_304@msn.com>
2016-11-20 00:26:45 +0000

    linked chart to food array item

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -20,6 +20,8 @@ function MainController(moment, Food, User, $auth, $state, $rootScope) {
 
   //this function gets just this current users foods from all existing foods. pushes them to main.allMyFoods
   function getFoods() {
+    main.caloryCounter = 0;
+    main.allMyFoods = [];
     for(let j=0; j<main.allFood.length; j++) {
       if(thisUser.eaten.indexOf(main.allFood[j]._id) !== -1) {
         main.allMyFoods.push(main.allFood[j]);
@@ -63,34 +65,58 @@ function MainController(moment, Food, User, $auth, $state, $rootScope) {
 
   main.logout = logout;
 
-  // main.createChart();
-}
+
+
+
+  main.createChart = createChart;
 
 // COMMENTED OUT PIE CHART DATA FOR REFFERENCE
-// data = {
-//   labels: ['Fibre', 'Protein', 'Carbohydrate', 'Fats', 'Water'],
-//   datasets: [{
-//     label: 'Foods',
-//     data: [12, 30, 200, 20, 40],
-//     backgroundColor: [
-//       'rgba(255, 99, 132, 0.4)',
-//       'rgba(54, 162, 235, 0.4)',
-//       'rgba(255, 206, 86, 0.4)',
-//       'rgba(75, 192, 192, 0.4)',
-//       'rgba(153, 102, 255, 0.4)',
-//       'rgba(255, 159, 64, 0.4)'
-//     ],
-//     borderColor: [
-//       'rgba(255,99,132,1)',
-//       'rgba(54, 162, 235, 1)',
-//       'rgba(255, 206, 86, 1)',
-//       'rgba(75, 192, 192, 1)',
-//       'rgba(153, 102, 255, 1)',
-//       'rgba(255, 159, 64, 1)'
-//     ],
-//     borderWidth: 1
-//   }]
-// };
+  function createChart() {
+    todaysCals();
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [moment().subtract(4, 'days').format('DD/MM/YYYY'), moment().subtract(3, 'days').format('DD/MM/YYYY'), moment().subtract(2, 'days').format('DD/MM/YYYY'), moment().subtract(1, 'days').format('DD/MM/YYYY'), 'Today'],
+            datasets: [{
+                label: '# of Votes',
+                data: [1500, 2000, 3000, 2500, main.caloryCounter],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+  }
+}
+
+
+// tesco primary api key: a3e5d8d2bece47679e56b9e289f3c6b3
+// secondary key: 084af90bdc7e4569975087b403b43bcc
+
 
 CountdownController.$inject = [];
 function CountdownController() {
